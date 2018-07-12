7424784074eb54308709354ac913679d1a59d37d
spartchou <spart.chou@gmail.com>
2018-07-13 01:42:17 +0800

    Add the author association

--- blorgh/app/models/blorgh/article.rb
+++ blorgh/app/models/blorgh/article.rb
@@ -1,5 +1,14 @@
 module Blorgh
   class Article < ActiveRecord::Base
     has_many :comments
+    belongs_to :author, class_name: "User"
+
+    before_validation :set_author
+
+    private
+
+    def set_author
+      self.author = User.find_or_create_by(name: author_name)
+    end
   end
 end
