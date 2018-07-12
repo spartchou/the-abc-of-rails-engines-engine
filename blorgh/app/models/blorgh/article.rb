48fc54dbbc94e2299117d1199bb78e7545017101
spartchou <spart.chou@gmail.com>
2018-07-13 02:15:51 +0800

    Configuring an Engine

--- blorgh/app/models/blorgh/article.rb
+++ blorgh/app/models/blorgh/article.rb
@@ -2,14 +2,13 @@ module Blorgh
   class Article < ActiveRecord::Base
     attr_accessor :author_name
     has_many :comments
-    belongs_to :author, class_name: "User"
-
+    belongs_to :author, class_name: Blorgh.author_class.to_s
     before_validation :set_author
 
     private
 
     def set_author
-      self.author = User.find_or_create_by(name: author_name)
+      self.author = Blorgh.author_class.find_or_create_by(name: author_name)
     end
   end
 end
